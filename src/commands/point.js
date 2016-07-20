e6f73f0e2dc9bbefca2fcbfad3c52a876dcfbb58
jpage <jpage@godaddy.com>
2016-07-20 09:06:27 -0700

    Address lookup

--- src/commands/point.js
+++ src/commands/point.js
@@ -1,13 +1,36 @@
+import Promise from 'bluebird';
+import { resolve } from 'dns';
 import readHostFile from '../read-host-file';
 import writeHostFile from '../write-host-file';
 import lineMatchesHost from '../line-matches-host';
 import backupHosts from '../backup-hosts';
+import { addressRegex } from '../patterns';
 
 export default {
   command: 'point <host> to <address>',
   describe: 'Assigns an address to the given hostname',
   handler({ host, address }) {
-    readHostFile().then(lines => {
+
+    let destHostName = null;
+    const resolveDest = addressRegex.test(address)
+      ? Promise.resolve()
+      : Promise.fromCallback(cb => {
+          return resolve(address, cb)
+        }).then(addresses => {
+          const validAddress = addresses.find(addr => addressRegex.test(addr));
+          if (!validAddress) {
+            throw new Error(`Could not resolve ${address}.`);
+          }
+
+          destHostName = address;
+          address = validAddress;
+        });
+
+    return (
+      resolveDest
+    ).then(() => {
+      return readHostFile();
+    }).then(lines => {
       let updated = false;
       let foundMatch = false;
       let insertionPoint = lines.length;
@@ -41,6 +64,7 @@ export default {
             isActive: true,
             address,
             hosts: [host],
+            comment: destHostName && `# ${destHostName}`,
             updated: true
           });
         }
