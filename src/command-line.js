0154486848114cda96042685f7a23043b5201092
jpage <jpage@godaddy.com>
2016-07-20 09:12:52 -0700

    Improve help

--- src/command-line.js
+++ src/command-line.js
@@ -3,6 +3,8 @@ import yargs from 'yargs';
 export default function execute() {
   return yargs
     .commandDir('commands', { visit: module => module.default })
+    .demand(1)
+    .strict()
     .help()
     .argv;
 }
